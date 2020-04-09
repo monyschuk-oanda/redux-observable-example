853a401cdf877f9bc3be222ac2bbdf57c8d6ad63
Mark Onyschuk <mark.onyschuk@gmail.com>
2020-04-09 06:32:14 -0400

    update: workshop start state

--- app/epics/quotes.js
+++ app/epics/quotes.js
@@ -1,53 +1,31 @@
 import * as fromQuotes from '../actions/quotes';
 
 import { ajax } from 'rxjs/ajax';
-import { of } from 'rxjs';
-import { delay, tap, map, switchMap, mergeMap, catchError, distinctUntilKeyChanged } from 'rxjs/operators';
+import { of, empty } from 'rxjs';
+import { delay, tap, map, switchMap, mergeMap, catchError, distinctUntilKeyChanged, mapTo } from 'rxjs/operators';
 
 export const fetchQuotes = (action$) => 
   action$.ofType(fromQuotes.FETCH_QUOTES).pipe(
-    switchMap(() => 
-      fetchQ().pipe(
-        map((result) => result.response),
-        map(fromQuotes.loadQuotes),
-        catchError((err) => console.log(err)),
-      )),
+    mergeMap(() => empty())
   );
 
 export const saveQuote = (action$) =>
   action$.ofType(fromQuotes.SAVE_QUOTE).pipe(
-    tap((action) => `saveQuote: ${action}`),
-    map((action) => action.payload),
-    distinctUntilKeyChanged("text"),
-    switchMap((quote) =>
-      insertQ(quote).pipe(
-        map((result) => result.response),
-        map(fromQuotes.loadQuote),
-      )), 
+    mergeMap(() => empty())
   );
 
 export const toggleFavorite = (action$) =>
   action$.ofType(fromQuotes.TOGGLE_FAVORITE).pipe(
-    map((action) => action.payload),
-    switchMap(({ _id, text, favorited }) => 
-      updateQ(_id, text, !favorited).pipe(
-        map((result) => result.response),
-        map(() => fromQuotes.loadQuote({_id, text, favorited: !favorited})),
-      )),
+    mergeMap(() => empty())
   );
 
 export const deleteQuote = (action$) =>
   action$.ofType(fromQuotes.DELETE_QUOTE).pipe(
-      map((action) => action.payload),
-      switchMap((quote) => 
-        deleteQ(quote._id).pipe(
-          map((result) => result.response),
-          map(() => fromQuotes.deleteSuccess(quote._id)),
-        )),
+    mergeMap(() => empty())
   );
 
 // API endpoints
 const fetchQ = () => ajax.get(`/api/quotes`)
 const insertQ = (quote) => ajax.post(`/api/quotes`, quote)
 const deleteQ = (id) => ajax.delete(`/api/quotes/${id}`)
-const updateQ = (id, text, favorited) => of({}).pipe(delay(1000), mergeMap(() => ajax.patch(`/api/quotes/${id}`, { _id: id, text, favorited })))
+const updateQ = (id, text, favorited) => ajax.patch(`/api/quotes/${id}`, { _id: id, text, favorited })
