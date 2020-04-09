9b469cc8d510746adec9b1dbffc40f56d4c37ff9
Mark Onyschuk <mark.onyschuk@gmail.com>
2020-04-09 04:38:42 -0400

    update: quotes api to rxjs6.x

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <div className="container">
-      <a className="pull-right" href="https://github.com/monad98">GitHub Repos</a>
-      <p>© 2017 Hyungwu Pae All Rights Reserved</p>
+      <a className="pull-right" href="https://github.com/monyschuk-oanda/redux-observable-example">GitHub Repo</a>
+      <p>© 2020 OANDA Corporation, All Rights Reserved</p>
+      <p>Original rxjs-5.x, redux-observable-0.x, redux-3.x version © 2017 Hyungwu Pae All Rights Reserved</p>
     </div>
   </footer>
 );
